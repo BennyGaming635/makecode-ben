3ad4e96743c229cc5c37e7c2ad275f01096d362f
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2025-05-28 13:56:22 +0930

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image3":
+            case "yes":return img`
+7 . . . 7 . 7 7 7 7 . 7 7 7 7 7 
+7 . . . 7 . 7 . . . . 7 . . . . 
+. 7 . 7 . . 7 . . . . 7 . . . . 
+. 7 . 7 . . 7 . . . . 7 . . . . 
+. . 7 . . . 7 . . . . 7 . . . . 
+. . 7 . . . 7 . . . . 7 . . . . 
+. . 7 . . . 7 . . . . 7 . . . . 
+. . 7 . . . 7 7 7 . . 7 7 7 7 7 
+. . 7 . . . 7 . . . . . . . . 7 
+. . 7 . . . 7 . . . . . . . . 7 
+. . 7 . . . 7 . . . . . . . . 7 
+. . 7 . . . 7 . . . . . . . . 7 
+. . 7 . . . 7 . . . . . . . . 7 
+. . 7 . . . 7 . . . . . . . . 7 
+. . 7 . . . 7 . . . . . . . . 7 
+. . 7 . . . 7 7 7 7 . 7 7 7 7 7 
+`;
+            case "image1":
+            case "no red":return img`
+2 2 2 2 2 . . . . . 2 2 2 2 2 2 
+2 3 3 3 2 . . . . . 2 3 2 3 3 3 
+2 3 2 3 2 2 . . . . 2 3 2 3 2 3 
+2 3 2 2 3 2 . . . . 2 3 2 3 2 3 
+2 3 2 2 3 2 2 . . . 2 3 2 3 2 3 
+2 3 2 2 2 3 2 . . . 2 3 2 3 2 3 
+2 3 2 . 2 3 2 2 . . 2 3 2 3 2 3 
+2 3 2 . 2 2 3 2 . . 2 3 2 3 2 3 
+2 3 2 . . 2 3 2 2 . 2 3 2 3 2 3 
+2 3 2 . . 2 2 3 2 . 2 3 2 3 2 3 
+2 3 2 . . . 2 3 2 2 2 3 2 3 2 3 
+2 3 2 . . . 2 2 3 2 2 3 2 3 2 3 
+2 3 2 . . . . 2 3 2 2 3 2 3 2 3 
+2 3 2 . . . . 2 2 3 2 3 2 3 2 3 
+2 3 2 . . . . . 2 3 3 3 2 3 3 3 
+2 2 2 . . . . . 2 2 2 2 2 2 2 2 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
